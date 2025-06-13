9d2ee3ac0d4bc2df173f51e6294677d9b80f3fc5
Jdsatashi <vdt1073@gmail.com>
2025-06-13 12:06:14 +0700

    Add comment to code

--- src/services/perm_services.py
+++ src/services/perm_services.py
@@ -12,16 +12,22 @@ async def get_perms(db: AsyncSession, params: QueryParams):
 
 
 async def create_perm(db: AsyncSession, perm_data: AddPemRequest) -> Permission | str:
-    is_valid, err = validate_perm_data(perm_data)
+    """ Create a new permission in the database."""
+    is_valid, err = validate_perm_data(db, perm_data)
     if not is_valid:
         return err
+    # Generate permission name based on action, model name, and option object primary key.
     perm_name = f"{perm_data.action}_{perm_data.model_name}"
     if perm_data.object_pk:
         perm_name += f"_{perm_data.object_pk}"
+    
+    # Check if the permission already exists
     result = await db.execute(select(exists().where(Permission.name == perm_name)))
-
+    # If the permission already exists, return an error message
     if result.scalar():
         return f"Permission {perm_name} already exists."
+    
+    # Create a new permission
     new_perm = Permission(
         name=perm_name,
         description=perm_data.description,
@@ -29,22 +35,26 @@ async def create_perm(db: AsyncSession, perm_data: AddPemRequest) -> Permission 
         object_pk=perm_data.object_pk,
         model_name=perm_data.model_name
     )
+    
     db.add(new_perm)
+    # Commit changes and refresh data
     await db.commit()
     await db.refresh(new_perm)
     return new_perm
 
 
 async def validate_perm_data(db: AsyncSession, perm_data: AddPemRequest) -> tuple[bool, str | None]:
+    """ Validate the permission data before creating a new permission."""
+    model_name = perm_data.model_name
     # Check if models String is valid
-    if perm_data.model_name not in list(MODEL_REGISTRY.keys()):
-        return False, f"Model {perm_data.model_name} does not exist in the registry."
+    if model_name not in list(MODEL_REGISTRY.keys()):
+        return False, f"Model {model_name} does not exist in the registry."
     # Check primary key of the model
     if perm_data.object_pk:
         # Get models class
-        model = MODEL_REGISTRY[perm_data.model_name]
+        model = MODEL_REGISTRY[model_name]
         obj = await db.get(model, perm_data.object_pk)
         # Check if the model has the primary key specified
         if obj is None:
-            return False, f"Model {perm_data.model_name} does not have a primary key named {perm_data.object_pk}."
+            return False, f"Model {model_name} does not have a primary key named {perm_data.object_pk}."
     return True, None
