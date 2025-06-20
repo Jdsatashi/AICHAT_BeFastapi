6c362056e75f5c7ff4d20f8af4562c2b8dea5b5b
Jdsatashi <vdt1073@gmail.com>
2025-06-20 21:30:56 +0700

    Add default time expire for create token

--- src/handlers/jwt_token.py
+++ src/handlers/jwt_token.py
@@ -1,12 +1,15 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 
 from jose import jwt, JWTError
 
 from src.conf import settings
 from src.schema.auth_schema import TokenPayload
+from src.utils.unow import now_vn
 
 
-def create_access_token(data: dict, expire) -> str:
+def create_access_token(data: dict, expire = None) -> str:
+    if expire is None:
+        expire = now_vn() + timedelta(minutes=settings.JWT_ACCESS_TOKEN_EXPIRE_MINUTES)
     to_encode = data.copy()
     to_encode.update({"exp": expire, "type": "access"})
     
@@ -14,7 +17,9 @@ def create_access_token(data: dict, expire) -> str:
     
     return jwt.encode(to_encode, settings.JWT_SECRET_KEY, algorithm=settings.JWT_ALGORITHM)
 
-def create_refresh_token(data: dict, expire) -> str:
+def create_refresh_token(data: dict, expire = None) -> str:
+    if expire is None:
+        expire = now_vn() + timedelta(days=settings.JWT_REFRESH_TOKEN_EXPIRE_DAY)
     to_encode = data.copy()
     to_encode.update({"exp": expire, "type": "refresh"})
     return jwt.encode(to_encode, settings.JWT_SECRET_KEY, algorithm=settings.JWT_ALGORITHM)
