4f0ce39b7892daa90551bb9cc1c6c17d95343b2d
Jdsatashi <vdt1073@gmail.com>
2025-06-30 22:22:37 +0700

    Edit split get_perm_name string, generate_perm by perm object and rebase create_all_perms

--- src/handlers/perm.py
+++ src/handlers/perm.py
@@ -4,27 +4,37 @@ from src.models import Permission
 from src.utils.perm_actions import actions_list
 
 
-async def create_all_perms(model_name, obj_id = None, depend_on=None, db: AsyncSession = None) -> list[str]:
+async def create_all_perms(model_name: str, obj_id=None, depend_on=None, db: AsyncSession = None) -> list[str]:
     """ Auto create permissions for the system. """
     all_perm_name = list()
     for action in actions_list:
         # Create permission name and description
-        name = f"{action}_{model_name}"
-        all_perm_name.append(name)
-        description = f"{action.upper()} permission on {model_name}"
-        if obj_id is not None:
-            name = f"{name}_{obj_id}"
-            description = f"{description} for object ID {obj_id}"
+        all_perm_name.append(get_perm_name(action, model_name, obj_id))
         if db is not None:
-            try:
-                db.add(Permission(
-                    name=name,
-                    description=description,
-                    model_name=model_name,
-                ))
-                # Apply the changes to the database
-                await db.commit()
-            except Exception as e:
-                await db.rollback()
-                raise e
+            perm = await generate_perm(model_name, action, obj_id, depend_on, db)
+            db.add(perm)
+
     return all_perm_name
+
+
+def get_perm_name(action: str, model_name: str, obj_id: int | None = None) -> str:
+    """ Get permission name by action and model name. """
+    if obj_id is not None:
+        return f"{action}_{model_name}_{obj_id}"
+    return f"{action}_{model_name}"
+
+
+async def generate_perm(model_name: str, action: str, obj_id: int | None = None,
+                        depend_on: str | None = None, db: AsyncSession = None) -> Permission:
+    """ Create a single permission in the database. """
+    perm_name = get_perm_name(action, model_name, obj_id)
+    description = f"{action.upper()} permission on {model_name}"
+    if obj_id is not None or obj_id != "":
+        description = f"{description} for object ID {obj_id}"
+    return Permission(
+        name=perm_name,
+        description=description,
+        object_pk=obj_id,
+        model_name=model_name,
+        depend_on=depend_on if depend_on != '' else None
+    )
