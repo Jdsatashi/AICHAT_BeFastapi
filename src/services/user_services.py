67f4f2e6f796e7245ca8fa8a0b15a7228186fadb
Jdsatashi <vdt1073@gmail.com>
2025-07-03 10:37:04 +0700

    Add try catch and roll back when error

--- src/services/user_services.py
+++ src/services/user_services.py
@@ -10,6 +10,7 @@ from src.schema.user_schema import UserCreate, UserSelfUpdate, ChangePassword
 from src.services.generic_services import get_all
 from src.services.perm_services import create_main_perms
 from src.utils.err_msg import err_msg
+from src.utils.logs import debug_log
 
 
 async def get_users(db: AsyncSession, params: QueryParams):
@@ -18,35 +19,49 @@ async def get_users(db: AsyncSession, params: QueryParams):
 
 async def create_user(db: AsyncSession, user_data: UserCreate) -> Users:
     """ Function create new user with username, email and password """
-    # Get password to hash
-    password = hash_pass(user_data.password)
-    # Init Users object
-    new_user = Users(
-        username=user_data.username,
-        email=str(user_data.email),
-        password=password
-    )
-    # Assign add user to db
-    db.add(new_user)
-    # Flush to get user id before creating permissions and roles
-    await db.flush()
+    try:
+        # Get password to hash
+        password = hash_pass(user_data.password)
+        # Init Users object
+        new_user = Users(
+            username=user_data.username,
+            email=str(user_data.email),
+            password=password
+        )
+        # Assign add user to db
+        db.add(new_user)
+        # Flush to get user id before creating permissions and roles
+        await db.flush()
 
-    permissions = await create_main_perms(db, Users.__name__, new_user.id)
- 
-    await db.flush()  # Để permissions có id nếu cần
+        # Create 4 main permissions for the user
+        permissions = await create_main_perms(db, Users.__name__, new_user.id)
 
-    new_role = Role(
-        name=f"{Users.__name__}_{new_user.id}",
-        description=f"Auto role for user {new_user.id}",
-        group=False,
-        is_active=True,
-        permissions=permissions
-    )
-    db.add(new_role)
-    await db.refresh(new_user, attribute_names=["roles"])
-    new_user.roles.append(new_role)
-    await db.commit()
-    return new_user
+        # Ensure permissions are created before role
+        await db.flush()
+
+        # Create Object role
+        new_role = Role(
+            name=f"{Users.__name__}_{new_user.id}",
+            description=f"Auto role for user {new_user.id}",
+            group=False,
+            is_active=True,
+            permissions=permissions
+        )
+        db.add(new_role)
+        # Refresh user to get user id and preload roles
+        await db.refresh(new_user, attribute_names=["roles"])
+
+        # Append new role to user roles
+        new_user.roles.append(new_role)
+
+        # Commit all changes to db
+        await db.commit()
+        return new_user
+    except Exception as e:
+        # If error occurred, rollback all changes
+        await db.rollback()
+        debug_log(f"Error when creating user: \n{e}")
+        return err_msg.internal_error
 
 
 async def get_user(db: AsyncSession, user_id: int) -> Type[Users] | None:
@@ -66,11 +81,17 @@ async def update_user(db: AsyncSession, user_id: int, user_data: UserSelfUpdate)
     # Assign new email if inputted
     if user_data.email:
         user.email = str(user_data.email)
-    # Commit change to db
-    await db.commit()
-    # Refresh to get new update of user
-    await db.refresh(user)
-    return user
+    try:
+        # Commit change to db
+        await db.commit()
+        # Refresh to get new update of user
+        await db.refresh(user)
+        return user
+    except Exception as e:
+        # If error occurred, rollback all changes
+        await db.rollback()
+        debug_log(f"Error when updating user profile: \n{e}")
+        return err_msg.internal_error
 
 
 async def delete_user(db: AsyncSession, user_id: int) -> None | str:
@@ -105,9 +126,14 @@ async def change_password(db: AsyncSession, user_id: int, user_data: ChangePassw
     # Hash new password and assign to user object
     user.password = hash_pass(user_data.new_password)
 
-    # Commit change
-    await db.commit()
-    # Refresh to get newest data of object user
-    await db.refresh(user)
-    # Return None if not error
-    return None
+    try:
+        # Commit change to db
+        await db.commit()
+        # Refresh to get new update of user
+        await db.refresh(user)
+        return None
+    except Exception as e:
+        # If error occurred, rollback all changes
+        await db.rollback()
+        debug_log(f"Error when updating user profile: \n{e}")
+        return err_msg.internal_error
