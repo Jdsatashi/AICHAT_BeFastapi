c1aafb8b5e9f6aa20b5aa1e81f03ad364df23626
Jdsatashi <vdt1073@gmail.com>
2025-07-03 10:36:32 +0700

    Add log debug when in debug mode

--- src/dependencies/middlewares.py
+++ src/dependencies/middlewares.py
@@ -13,19 +13,21 @@ from src.handlers.perm import get_perm_name
 from src.models import Users, Permission, Role
 from src.services.auth_services import check_access_token
 from src.utils.api_path import RoutePaths, route_model_map, route_model_pk_map
+from src.utils.logs import debug_log
 from src.utils.perm_actions import method_map, actions
 
 
 class PermissionMiddleware(BaseHTTPMiddleware):
     async def dispatch(self, request: Request, call_next):
+        debug_log(" -- Middleware function -- ")
         start_time = time.time()
 
         route_path = request.url.path
-        print(f"Request path: {route_path}")
+        debug_log(f"Request path: {route_path}")
 
         path_params = request.scope.get("path_params", {})
 
-        print(f"Path parameters: {path_params}")
+        debug_log(f"Path parameters: {path_params}")
 
         if (route_path.startswith("/docs")
                 or route_path.startswith("/openapi.json")
@@ -34,13 +36,15 @@ class PermissionMiddleware(BaseHTTPMiddleware):
                 or not route_path.startswith(RoutePaths.API_PREFIX)
         ):
             # Skip permission check for documentation and API prefix
-            print(f"Time validation: {time.time() - start_time}")
+            debug_log(f"Time validation: {time.time() - start_time}")
+            debug_log("Skipping permission for API prefix")
             return await call_next(request)
 
         # Check for Authorization header
         auth_header = request.headers.get("Authorization")
         if not auth_header or not auth_header.startswith("Bearer "):
-            print(f"Time validation: {time.time() - start_time}")
+            debug_log(f"Time validation: {time.time() - start_time}")
+            debug_log("Missing or invalid Authorization header")
             return JSONResponse(
                 status_code=HTTP_403_FORBIDDEN,
                 content={"detail": "Missing or invalid Authorization header"}
@@ -54,7 +58,8 @@ class PermissionMiddleware(BaseHTTPMiddleware):
             # Check access token and decode to get payload
             payload = await check_access_token(token, db)
             if isinstance(payload, str):
-                print(f"Time validation: {time.time() - start_time}")
+                debug_log(f"Time validation: {time.time() - start_time}")
+                debug_log(f"Invalid access token in payload")
                 return JSONResponse(
                     status_code=HTTP_403_FORBIDDEN,
                     content={"detail": f"Access token {payload}"}
@@ -65,7 +70,8 @@ class PermissionMiddleware(BaseHTTPMiddleware):
 
             # If user_id is not present in the payload, raise an error
             if not user_id:
-                print(f"Time validation: {time.time() - start_time}")
+                debug_log(f"Time validation: {time.time() - start_time}")
+                debug_log("User ID not found in token payload")
                 return JSONResponse(
                     status_code=HTTP_403_FORBIDDEN,
                     content={"detail": "Invalid data payload"}
@@ -80,19 +86,21 @@ class PermissionMiddleware(BaseHTTPMiddleware):
             )
             user = get_user.scalar_one_or_none()
             if not user:
-                print(f"Time validation: {time.time() - start_time}")
+                debug_log(f"Time validation: {time.time() - start_time}")
+                debug_log("User not found in database")
                 return JSONResponse(
                     status_code=HTTP_403_FORBIDDEN,
                     content={"detail": "User not found"}
                 )
 
             if user.is_active is False:
-                print(f"Time validation: {time.time() - start_time}")
+                debug_log(f"Time validation: {time.time() - start_time}")
+                debug_log("User is inactive")
                 return JSONResponse(
                     status_code=HTTP_403_FORBIDDEN,
                     content={"detail": "User is inactive"}
                 )
-
+            debug_log(f"User: {user.roles}")
             permissions = set()
             for role in user.roles:
                 for perm in role.permissions:
@@ -105,33 +113,37 @@ class PermissionMiddleware(BaseHTTPMiddleware):
         clean_path = clean_route_path(route_path)
 
         model_name, object_id = extract_model_and_object_id(clean_path)
-        print(object_id)
+        debug_log(object_id)
         if not model_name:
-            print(f"Time validation: {time.time() - start_time}")
+            debug_log(f"Time validation: {time.time() - start_time}")
             return await call_next(request)
 
         action = method_map[method]
-        print(permissions)
+        debug_log(permissions)
         if check_all_perm(model_name, action, permissions):
             return await call_next(request)
-        permission_needed = get_perm_name(model_name, action)
+        permission_needed = get_perm_name(model_name, action, object_id)
+        debug_log(f"Permission needed: {permission_needed}")
         if permission_needed in permissions:
-            depend_on = await get_permission_depend_on(permission_needed)
+            depend_on = await get_permission_depend_on(db, permission_needed)
+            debug_log(f"Depend on permission: {depend_on}")
             if depend_on:
                 if depend_on not in permissions:
-                    print(f"Time validation: {time.time() - start_time}")
+                    debug_log(f"Permission depend: {depend_on}")
+                    debug_log(f"Time validation: {time.time() - start_time}")
                     return JSONResponse(
                         status_code=HTTP_403_FORBIDDEN,
                         content={"detail": f"Permission {depend_on} is required"}
                     )
-            print(f"Time validation: {time.time() - start_time}")
+            debug_log(f"Time validation: {time.time() - start_time}")
             return await call_next(request)
 
-        print(f"Time validation: {time.time() - start_time}")
+        debug_log(f"Time validation: {time.time() - start_time}")
         return JSONResponse(
             status_code=HTTP_403_FORBIDDEN,
             content={"detail": f"Permission {method_map[method]} on {model_name} is required"}
         )
+
 
 def extract_model_and_object_id(route_path: str) -> tuple[str | None, int | None]:
     for pattern, model_name, param_key in route_model_pk_map:
